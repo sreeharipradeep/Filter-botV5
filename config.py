01bc1e8144a373905ade7fb4a016772bd29acc57
sreeharipradeep <83856203+sreeharipradeep@users.noreply.github.com>
2025-12-28 17:03:46 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+MONGO_URI = os.getenv("MONGO_URI")
+
+ADMINS = [123456789]  # നിങ്ങളുടെ Telegram ID
+
+START_PICS = [
+    "https://i.imgur.com/1.jpg",
+    "https://i.imgur.com/2.jpg"
+]
+
+CHANNEL = "https://t.me/yourchannel"
+GROUP = "https://t.me/yourgroup"
+COMMUNITY = "https://t.me/yourcommunity"
+BOT_USERNAME = "YourBotUsername"
