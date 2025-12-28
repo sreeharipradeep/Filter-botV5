e56dabf1beae7633ca0e5f4f7422673012629c8f
sreeharipradeep <83856203+sreeharipradeep@users.noreply.github.com>
2025-12-28 17:59:17 +0530

    Update confignew.py

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 import os
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-MONGO_URI = os.getenv("MONGO_URI")
+API_ID = int(os.getenv("36360287"))
+API_HASH = os.getenv("4283a743d393b1094600a7065a50b3c0")
+BOT_TOKEN = os.getenv("8518993193:AAG34Sa_JEilJGIQ5r02hOyqwF8Pzqcn8ZM")
+MONGO_URI = os.getenv("mongodb+srv://Mreldro:mreldro@cluster0.wdjsw5c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-ADMINS = [123456789]  # നിങ്ങളുടെ Telegram ID
+ADMINS = [8417661273]  # നിങ്ങളുടെ Telegram ID
 
 START_PICS = [
-    "https://i.imgur.com/1.jpg",
-    "https://i.imgur.com/2.jpg"
+    "https://graph.org/file/62386b57bf0394d7bd917-959daf5976f788890f.jpg",
+    "https://graph.org/file/dbc886d8bb5922d2ac2a6-7a3bbc2919ef5588a6.jpg"
 ]
 
-CHANNEL = "https://t.me/yourchannel"
-GROUP = "https://t.me/yourgroup"
-COMMUNITY = "https://t.me/yourcommunity"
-BOT_USERNAME = "YourBotUsername"
+CHANNEL = "https://t.me/+F2nvRjDLzhIzMjg9"
+GROUP = "https://t.me/trixel_movies"
+COMMUNITY = "https://t.me/jb_links"
+BOT_USERNAME = "@tony_stark_v3_bot"
